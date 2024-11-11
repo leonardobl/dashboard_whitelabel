bcfb557bdf55ed60c327d14a1201ddebf66e2ba0
Leonardo Lima <leonardo@paguextech.com>
2024-11-11 15:39:00 -0300

    criacao bg login

--- src/Components/Templates/LoginTemplate/index.tsx
+++ src/Components/Templates/LoginTemplate/index.tsx
@@ -2,15 +2,17 @@ import { Input } from "../../Atoms/Input";
 
 export const LoginTemplate = () => {
   return (
-    <div className="wrapper">
-      <form>
-        <Input
-          label="Label"
-          required
-          placeholder="placeholder"
-          iconright="/assets/svg/icon-cancel.svg"
-        />
-      </form>
+    <div className="page">
+      <div className="wrapper">
+        <form>
+          <Input
+            label="Label"
+            required
+            placeholder="placeholder"
+            iconright="/assets/svg/icon-cancel.svg"
+          />
+        </form>
+      </div>
     </div>
   );
 };
